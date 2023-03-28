ed25afee075f3c5a405c15b4164c950c85e4a65d
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-28 14:18:09 +0200

    add image

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,14 @@
-
 const { createApp } = Vue
 
-createApp({
+  createApp({
     data() {
-        return {
-            message: 'Hello Vue!'
-        }
+      return {
+        message: 'Hello Vue!',
+        img:{
+            class: 'image',
+            src: './asset/img/img.jpeg',
+            alt: 'cat'
+        },
+      }
     }
-}).mount('#app')
+  }).mount('#app')
